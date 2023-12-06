1ffb7e225e3ba00368078ba88722ab5284a4823b
polly_stos <polly_stos@yahoo.com.br>
2023-12-06 17:28:47 -0300

    colocando audio

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -2,7 +2,7 @@ const state = {
     score: {
         playerScore: 0,
         computerScore: 0,
-        scoreBox: document.getElementById("score_box"),
+        scoreBox: document.getElementById("score_points"),
     },
     cardSprites: {
         avatar: document.getElementById("card-image"),
@@ -83,7 +83,6 @@ async function setCardsField(cardId) {
     await removeAllCardsImages();
 
     let computerCardId = await getRandomcardId();
-    console.log(computerCardId);
     state.fieldCards.player.style.display = "block";
     state.fieldCards.computer.style.display = "block";
 
@@ -92,28 +91,34 @@ async function setCardsField(cardId) {
 
     let duelResults = await checkDuelResults(cardId,computerCardId);
 
-    // await updateScore();
+    await updateScore();
     await drowButton(duelResults);
 }
 
 async function drowButton(text){
-    state.actions.button.innerText = text;
+    state.actions.button.innerText = text.toUpperCase();
     state.actions.button.style.display = "block";
 }
 
+async function updateScore(){
+    state.score.scoreBox.innerText = `Win: ${state.score.playerScore} | Lose: ${state.score.computerScore}`;
+}
+
 async function checkDuelResults(playerCardId,computerCardId){
-    let duelResults = "Empate";
+    let duelResults = "draw";
     let playerCard = cardData[playerCardId];
 
     if(playerCard.WinOf.includes(computerCardId)){
-        duelResults = "Ganhou";
+        duelResults = "win";
         state.score.playerScore++;
     }
-
+    
     if(playerCard.LoseOf.includes(computerCardId)){
-        duelResults = "Perdeu";
+        duelResults = "lose";
         state.score.computerScore++;
     }
+    await playAudio(duelResults);
+    
     return duelResults;
 }
 
@@ -140,10 +145,25 @@ async function drawCards(cardNumbers, fieldSide) {
         document.getElementById(fieldSide).appendChild(cardImage);
     }
 }
-// const players ={
-//     player1:"player-cards"
 
-// }
+async function resetDuel(){
+    state.cardSprites.avatar.src="";
+    state.actions.button.style.display = "none";
+
+    state.fieldCards.player.style.display = "none";
+    state.fieldCards.computer.style.display = "none";
+
+    init();
+}
+
+async function playAudio(status){
+    const audio = new Audio(`./src/assets/audios/${status}.wav`);
+
+    try{
+        audio.play();
+    }catch{}
+
+}
 
 function init() {
     drawCards(5, state.playerSides.player1);
